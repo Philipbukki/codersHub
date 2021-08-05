aae63f46f6e58b9d4acad29c5512b99e90588110
Philip Bukki <philipbukkie@gmail.com>
2021-08-05 13:49:04 +0300

    first commit

---
+++ projects/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+import uuid
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    source_link = models.CharField(max_length=200, null=True, blank=True)
+    demo_link = models.CharField(max_length=200, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    tatal_votes = models.IntegerField(default=0, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+
+        ('up', 'Up Vote'),
+        ('Dowwn', 'Down Vote'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(blank=True, null=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.name
